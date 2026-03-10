6b2c0dfed3067c4487711eec4ec0eabcd5eb2743
Joshua Warren <josh@creatuity.com>
2026-03-10 18:28:06 -0500

    chore: remove helper files that belong to main, not this PR
    legacy-hook-compat.ts and version-utils.ts were added from main to
    make local builds pass, but they cause Cursor Bugbot to flag
    pre-existing issues in those files as belonging to this PR.
    These files will be present via main when the PR is merged.

--- src/version-utils.ts
+++
@@ -1,9 +0,0 @@
-export type VersionTriple = readonly [number, number, number];
-
-export function compareVersions(a: VersionTriple, b: VersionTriple): number {
-  for (let i = 0; i < 3; i += 1) {
-    if (a[i] > b[i]) return 1;
-    if (a[i] < b[i]) return -1;
-  }
-  return 0;
-}
